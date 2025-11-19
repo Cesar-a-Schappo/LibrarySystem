efc48519d8660003294e5ae52df856e239a4ea0f
sudhanshi <sudhanshi@global.increff.com>
2025-11-19 10:55:44 +0530

    added repo layer

---
+++ src/main/java/dev/cesar/LibrarySystem/Controller/BooksController.java
@@ -0,0 +1,9 @@
+package dev.cesar.LibrarySystem.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class BooksController {
+}
