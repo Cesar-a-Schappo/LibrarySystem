63a221cf3165d4fe5180b286eba96596580ea4ed
Megacaquito <cesinha.schappo07@gmail.com>
2025-11-26 01:38:23 -0300

    feat: cria o ReadersRepository que extende o JpaRepository

---
+++ src/main/java/dev/cesar/LibrarySystem/Readers/ReadersRepository.java
@@ -0,0 +1,6 @@
+package dev.cesar.LibrarySystem.Readers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadersRepository extends JpaRepository<ReadersModel, Long> {
+}
