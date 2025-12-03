cbbbd6c2dd489b14db4ad1fef33a499cad5fc994
Megacaquito <cesinha.schappo07@gmail.com>
2025-12-03 14:46:49 -0300

    chore: inclui a versao do DTO pela entidade books e mapeia manualmente no BooksMapper os caminhos entidade x DTO

--- src/main/java/dev/cesar/LibrarySystem/Books/BooksDTO.java
+++ src/main/java/dev/cesar/LibrarySystem/Books/BooksDTO.java
@@ -1,4 +1,19 @@
 package dev.cesar.LibrarySystem.Books;
 
+import dev.cesar.LibrarySystem.Readers.ReadersModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BooksDTO {
+
+    private Long id;
+    private String title;
+    private String genre;
+    private String author;
+    private ReadersModel reader;
+
 }
