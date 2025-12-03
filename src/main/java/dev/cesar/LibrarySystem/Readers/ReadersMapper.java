18b3e2a56938624bfdd60ce9e8bdca9486b95cd9
Megacaquito <cesinha.schappo07@gmail.com>
2025-12-03 14:07:24 -0300

    chore: inclui a versao do DTO pela entidade readers e mapeia manualmente no ReadersMapper os caminhos entidade x DTO

--- src/main/java/dev/cesar/LibrarySystem/Readers/ReadersMapper.java
+++ src/main/java/dev/cesar/LibrarySystem/Readers/ReadersMapper.java
@@ -1,4 +1,32 @@
 package dev.cesar.LibrarySystem.Readers;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ReadersMapper {
+
+    public ReadersModel map(ReadersDTO readersDTO) {
+        ReadersModel readersModel = new ReadersModel();
+        readersModel.setId(readersDTO.getId());
+        readersModel.setName(readersDTO.getName());
+        readersModel.setEmail(readersDTO.getEmail());
+        readersModel.setAge(readersDTO.getAge());
+        readersModel.setAddress(readersDTO.getAddress());
+        readersModel.setBooks(readersDTO.getBooks());
+
+        return readersModel;
+    }
+
+    public ReadersDTO map(ReadersModel readersModel) {
+        ReadersDTO readersDTO = new ReadersDTO();
+        readersDTO.setId(readersModel.getId());
+        readersDTO.setName(readersModel.getName());
+        readersDTO.setEmail(readersModel.getEmail());
+        readersDTO.setAge(readersModel.getAge());
+        readersDTO.setAddress(readersModel.getAddress());
+        readersDTO.setBooks(readersModel.getBooks());
+
+        return readersDTO;
+    }
+
 }
