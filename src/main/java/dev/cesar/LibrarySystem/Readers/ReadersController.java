62c6c99e04ad55e1db7e9303055c5b0231f314be
Megacaquito <cesinha.schappo07@gmail.com>
2025-12-15 21:26:23 -0300

    doc: documenta as rotas de ReadersController

--- src/main/java/dev/cesar/LibrarySystem/Readers/ReadersController.java
+++ src/main/java/dev/cesar/LibrarySystem/Readers/ReadersController.java
@@ -1,5 +1,9 @@
 package dev.cesar.LibrarySystem.Readers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,20 +21,34 @@ public class ReadersController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<String> createReader(@RequestBody ReadersDTO reader) {
+    @Operation(summary = "Create a new reader", description = "This route creates a new reader and inserts it on the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "reader successfully created"),
+            @ApiResponse(responseCode = "400", description = "reader creation error")
+    })
+    public ResponseEntity<String> createReader(
+            @Parameter(description = "user send the data of the reader that will be created on the path request") @RequestBody ReadersDTO reader) {
         ReadersDTO newReader = readersService.createReader(reader);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Reader successfully created: " + newReader.getName() + ". ID: " + newReader.getId());
     }
 
     @GetMapping("/list")
+    @Operation(summary = "List all readers", description = "This route lists all the readers in the database")
+    @ApiResponse(responseCode = "200", description = "readers listed")
     public ResponseEntity<List<ReadersDTO>> listAllReaders() {
         List<ReadersDTO> readers = readersService.listAllReaders();
         return ResponseEntity.ok(readers);
     }
 
     @GetMapping("/list/{id}")
-    public ResponseEntity<?> listReadersById(@PathVariable Long id) {
+    @Operation(summary = "List a reader by ID", description = "This route lists a reader by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "reader listed"),
+            @ApiResponse(responseCode = "404", description = "reader not found")
+    })
+    public ResponseEntity<?> listReadersById(
+            @Parameter(description = "user sends the ID of the reader that will be listed on the path request") @PathVariable Long id) {
         ReadersDTO reader = readersService.listReadersById(id);
         if (reader != null) {
             return ResponseEntity.status(HttpStatus.FOUND)
@@ -42,7 +60,14 @@ public class ReadersController {
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<String> updateReaderById(@PathVariable Long id, @RequestBody ReadersDTO reader) {
+    @Operation(summary = "Update a reader data", description = "This route gets a reader by its ID and updates its data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "reader updated"),
+            @ApiResponse(responseCode = "404", description = "reader not found. data not updated")
+    })
+    public ResponseEntity<String> updateReaderById(
+            @Parameter(description = "user sends the ID of the reader that will be updated on the path request") @PathVariable Long id,
+            @Parameter(description = "user sends the reader data to be updated on the request body") @RequestBody ReadersDTO reader) {
         ReadersDTO updatedReader = readersService.updateReaderById(id, reader);
         if (updatedReader != null) {
             return ResponseEntity.ok("Reader of ID: " + id + " updated.");
@@ -53,7 +78,13 @@ public class ReadersController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteReaderById(@PathVariable Long id) {
+    @Operation(summary = "Delete a reader", description = "This route gets a reader by its ID and deletes it from the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "reader deleted"),
+            @ApiResponse(responseCode = "404", description = "reader not found. reader not deleted")
+    })
+    public ResponseEntity<String> deleteReaderById(
+            @Parameter(description = "user sends the ID of the reader that will be deleted on the path request") @PathVariable Long id) {
         if (readersService.listReadersById(id) != null) {
             readersService.deleteReaderById(id);
             return ResponseEntity.ok("Reader of ID: " + id + " deleted.");
